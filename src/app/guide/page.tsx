943cd57131de9a35278109e6673cbccb585f8454
ichanddan <code.us.cm@gmail.com>
2024-02-13 01:06:04 +0530

    fix some css

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -14,13 +14,13 @@ import finalToken from "@/res/image/final token.png";
 function Guide() {
   return (
     <div className="mx-auto max-w-screen-lg">
-      <h1 className="text-center mt-5 font-bold font-7xl">
+      <h1 className="text-center mt-10 font-bold text-4xl md:text-6xl font-extra bold">
         How to create dummy-api <span className="text-red-700">Steps</span>{" "}
       </h1>
       <div className="mt-10">
         <center>
-          <div className="mt-5 items-end justify-end">
-            <h1 className="mb-2 font-bold">Step 1: Signup or Login</h1>
+          <div className="mt-12 items-end justify-end">
+            <h1 className="mb-2 font-bold text-2xl my-10">Step 1: Signup or Login</h1>
             <Image
               src={signUp}
               width={750}
@@ -28,12 +28,12 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md"
             />
-            <p className="my-2 text-lx">
+            <p className="my-2 text-xl">
               First create account on dummy-api.com and <br />
               Create project names and follow{" "}
               <span className="text-red-700">Step 2</span>{" "}
             </p>
-            <h1 className="my-5 font-bold">Step 2: Create project</h1>
+            <h1 className="mb-2 font-bold text-2xl my-10">Step 2: Create project</h1>
             <Image
               src={createproject}
               width={750}
@@ -41,7 +41,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After create Project and click your project name <br /> and
               follows <span className="text-red-700">Step 3</span>{" "}
             </p>
@@ -52,7 +52,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md"
             />
-            <h1 className="my-5 font-bold">Step 3: New EndPoint</h1>
+            <h1 className="mb-2 font-bold text-2xl my-10">Step 3: New EndPoint</h1>
             <Image
               src={endpoint}
               width={750}
@@ -60,7 +60,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After click your project name choice{" "}
               <span className="text-red-700">endpoint name</span> and <br />{" "}
               choice your Request Method{" "}
@@ -74,7 +74,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After choice your deta <span className="text-red-700">GET</span>{" "}
               or <span className="text-red-700">POST</span> Click on Add Field{" "}
             </p>
@@ -85,7 +85,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After Click on <span className="text-red-700">Add Field</span>{" "}
               choice your data acording your usegess <br />{" "}
               <span className="text-red-700">
@@ -99,11 +99,11 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After choice acording your usegess on Click{" "}
               <span className="text-red-700">Submit button</span>
             </p>
-            <h1 className="my-5 font-bold">Step 4: Copy Link & using</h1>
+            <h1 className="mb-2 font-bold text-2xl my-10">Step 4: Copy Link & using</h1>
             <Image
               src={apiLink}
               width={750}
@@ -111,7 +111,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">
+            <p className="my-2 text-xl">
               After <span className="text-red-700">Submit button</span> click 3
               dot and copy link
             </p>
@@ -122,7 +122,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">After copy your token id</p>
+            <p className="my-2 text-xl">After copy your token id</p>
             <Image
               src={finalToken}
               width={750}
@@ -130,7 +130,7 @@ function Guide() {
               alt="Picture of the author"
               className="rounded-md mt-4"
             />
-            <p className="my-2">And use api ezay way and fast</p>
+            <p className="my-2 text-xl">And use api ezay way and fast</p>
           </div>
         </center>
       </div>
